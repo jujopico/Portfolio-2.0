6babcb80f25545f5b025d691d86bfd2c64e3a4e8
jujopico <sujunqt@gmail.com>
2020-02-18 18:11:46 -0500

    organize structure

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /* land page animation */
-const homePageImage = document.querySelector('.home__page-image');
+const homePageImage = document.querySelector('.header__image');
 const slider = document.querySelector('.slider');
 
 const timeline = new TimelineMax();
