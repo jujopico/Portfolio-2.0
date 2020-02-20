a716184c99d986624065c2df5b8655d3b518a98e
jujopico <sujunqt@gmail.com>
2020-02-20 13:30:55 -0500

    css for nav links

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ navSlide();
 /* back to top button */
 const topBtn = document.querySelector(".btn--top");
 window.onscroll = function() {scrollFunction()};
-function scrollFunction() {
+const scrollFunction = () => {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     topBtn.style.display = "block";
   } else {
