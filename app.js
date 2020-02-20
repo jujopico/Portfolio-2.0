a6a5e1a730fdd3d9936b99edaed9a38c286550ea
jujopico <sujunqt@gmail.com>
2020-02-20 12:19:45 -0500

    add download resume button and back to top button

--- app.js
+++ app.js
@@ -46,3 +46,13 @@ const navSlide = () => {
 
 navSlide();
 
+/* back to top button */
+const topBtn = document.querySelector(".btn--top");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+}
